29b176e89f5a9cc3b2c4394c7c638b6e0b8c5cfa
dallinforsyth <dallfor192@gmail.com>
2023-01-19 11:40:26 -0600

    created working tests for controllers.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,12 +2,13 @@ class ProductsController < ApplicationController
   def show
     # @product = Product.first
     @product = Product.find_by(id: params[:id])
-     render :show
+
+    render :show
   end
-  
+
   def index
     @products = Product.all
+
     render :index
   end
- 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,10 @@ Rails.application.routes.draw do
   # get "/first_product_path/:id", controller: "products",
   # action: "first_product_method"
 
-  get "/first_product_path/:id" => "products#show"
+  get "/products/:id" => "products#show"
 
   # get "/all_product_path", controller: "products",
   # action: "all_product_method"
 
-  get "/all_product_path" => "products#index"
-
+  get "/products" => "products#index"
 end
