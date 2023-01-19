3692ebcd78e1915ac1fb7e577d7607c4ef88d50b
dallinforsyth <dallfor192@gmail.com>
2023-01-19 12:54:40 -0600

    added way to update a product and a test file to make sure it works!

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -21,4 +21,15 @@ class ProductsController < ApplicationController
     )
     render :show
   end
+
+  def update
+    @product = Product.find_by(id: params["id"])
+    @product.update(
+      name: params["name"] || @product.name,
+      price: params["price"] || @product.price,
+      image_url: params["iamge_url"] || @product.image_url,
+      description: params["description"] || @product.description,
+    )
+    render :show
+  end
 end
