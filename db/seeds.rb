52142c91dd37e7d6077a56eefc757c81e6960048
dallinforsyth <dallfor192@gmail.com>
2023-01-25 12:45:53 -0600

    added suppers db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,11 @@
 Product.create!([
-  {name: "Pencil", price: "15.0", image_url: "https://www.freeiconspng.com/thumbs/pencil-png/pencil-png-file-by-ama-chii-on-deviantart-14.png", description: "New number 2 pencil that can write up to 20 pages!", inventory: "7.0"},
-  {name: "Pencil", price: "15.0", image_url: "https://www.freeiconspng.com/thumbs/pencil-png/pencil-png-file-by-ama-chii-on-deviantart-14.png", description: "New number 2 pencil that can write up to 20 pages!", inventory: "8.0"},
-  {name: "pen", price: "5.0", image_url: "https://www.freeiconspng.com/thumbs/pencil-png/pencil-png-file-by-ama-chii-on-deviantart-14.png", description: "pen o pen", inventory: "8.0"},
-  {name: "Rock", price: "45.0", image_url: "https://www.freeiconspng.com/thumbs/pencil-png/pencil-png-file-by-ama-chii-on-deviantart-14.png", description: "a big ol papper weight", inventory: "4.0"},
-  {name: "Pencil", price: "15.0", image_url: "https://www.freeiconspng.com/thumbs/pencil-png/pencil-png-file-by-ama-chii-on-deviantart-14.png", description: "New number 2 pencil that can write up to 20 pages!", inventory: "9.0"},
-  {name: "Pencil", price: "15.0", image_url: "https://www.freeiconspng.com/thumbs/pencil-png/pencil-png-file-by-ama-chii-on-deviantart-14.png", description: "New number 2 pencil that can write up to 20 pages!", inventory: "5.0"},
-  {name: "bean", price: "2.0", image_url: "https://www.freeiconspng.com/thumbs/pencil-png/pencil-png-file-by-ama-chii-on-deviantart-14.png", description: "can o beans", inventory: "5.0"},
-  {name: "bag", price: "13.0", image_url: "https://www.freeiconspng.com/thumbs/pencil-png/pencil-png-file-by-ama-chii-on-deviantart-14.png", description: "bag o bag", inventory: "5.0"},
-  {name: "Pencil", price: "15.0", image_url: "https://www.freeiconspng.com/thumbs/pencil-png/pencil-png-file-by-ama-chii-on-deviantart-14.png", description: "New number 2 pencil that can write up to 20 pages!", inventory: "1.0"},
-  {name: "Pencil", price: "15.0", image_url: "https://www.freeiconspng.com/thumbs/pencil-png/pencil-png-file-by-ama-chii-on-deviantart-14.png", description: "New number 2 pencil that can write up to 20 pages!", inventory: "1.0"},
-  {name: "Pencil", price: "15.0", image_url: "https://www.freeiconspng.com/thumbs/pencil-png/pencil-png-file-by-ama-chii-on-deviantart-14.png", description: "New number 2 pencil that can write up to 20 pages!", inventory: "6.0"},
-  {name: "Pencil", price: "15.0", image_url: "https://www.freeiconspng.com/thumbs/pencil-png/pencil-png-file-by-ama-chii-on-deviantart-14.png", description: "New number 2 pencil that can write up to 20 pages!", inventory: "2.0"}
+
+  { name: "pen", price: "5.0", image_url: "https://www.freeiconspng.com/thumbs/pencil-png/pencil-png-file-by-ama-chii-on-deviantart-14.png", description: "pen with a whole lot pen o pen", inventory: "8.0" },
+  { name: "Rock", price: "45.0", image_url: "https://www.freeiconspng.com/thumbs/pencil-png/pencil-png-file-by-ama-chii-on-deviantart-14.png", description: "best purchus you will ever make a big ol papper weight", inventory: "4.0" },
+  { name: "Pencil", price: "15.0", image_url: "https://www.freeiconspng.com/thumbs/pencil-png/pencil-png-file-by-ama-chii-on-deviantart-14.png", description: "New number 2 pencil that can write up to 20 pages!", inventory: "9.0" },
+  { name: "bean", price: "2.0", image_url: "https://www.freeiconspng.com/thumbs/pencil-png/pencil-png-file-by-ama-chii-on-deviantart-14.png", description: "your favourte refiryed beans are back can o beans", inventory: "5.0" },
+  { name: "bag", price: "13.0", image_url: "https://www.freeiconspng.com/thumbs/pencil-png/pencil-png-file-by-ama-chii-on-deviantart-14.png", description: "best single strap bag that you could bag o bag", inventory: "5.0" },
 ])
+Supplier.create!(name: "Oakley", email: "oakley@test.com", phone_number: "9876543215")
+Supplier.create!(name: "Red Bull", email: "givesyouwings@test.com", phone_number: "5287419635")
+Supplier.create!(name: "Burton", email: "snowguys@test.com", phone_number: "8521463779")
